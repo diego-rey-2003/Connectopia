06814fb09529fc97afe6408d99aaa278eb666853
diego-rey-2003 <diego.rey2300@gmail.com>
2024-02-21 18:51:16 -0800

    finished first page

--- src/App.tsx
+++ src/App.tsx
@@ -2,63 +2,56 @@ import './index.css';
 
 function App() {
   return (
-    // Options Tab
-    // add links to the location when finished
-    
     <>
-      <div className="bg-green-400 h-20 flex items-center">
-        <button onClick={() => window.location.href = 'http://localhost:5173'} >
-          <h1 className="text-4xl text-white ml-6 font-serif">
-            Connectopia
-          </h1>
-        </button>
-
-        <button onClick={() => window.location.href = ''} className="flex-nowrap ml-80 font-serif hover:text-slate-950">
-          <h1 className="text-4xl text-white">
-            Browse
-          </h1>
-        </button>
-
-        <button onClick={() => window.location.href = ''} className="ml-14 font-serif">
-          <h1 className="text-4xl text-white">
-            Fun
-          </h1>
-        </button>
-
-        <button onClick={() => window.location.href = ''} className="ml-14 font-serif">
-          <h1 className="text-4xl text-white">
-            Authors
-          </h1>
-        </button>
-
-        <button onClick={() => window.location.href = ''} className="ml-14 font-serif">
-          <h1 className="text-4xl text-white">
-            Account
-          </h1>
-        </button>
-
-        <a className="group block rounded-lg p-1 bg-emerald-500 ring-slate-900/5 shadow-lg space-y-3 hover:bg-emerald-700 ml-14 transition-all">
-          <button onClick={() => window.location.href = ''}>
-            <h1 className="text-4xl text-white font-serif">
-              Subscribe
+      <div className='w-full h-screen bg-cover bg-no-repeat bg-left-bottom bg-fixed' style={{backgroundImage: "url('./src/assets/animated-light-bulb-gif.webp')"}}>
+        <div className="h-20 flex items-center">
+          <button onClick={() => window.location.href = 'http://localhost:5173'}>
+            <h1 className="text-4xl text-white ml-6 font-serif">
+              Connectopia
             </h1>
           </button>
-        </a>
+
+          <button onClick={() => window.location.href = ''} className="flex-nowrap ml-96 font-serif hover:text-slate-950 px-14">
+            <h1 className="text-4xl text-white hover:border-b-2 border-transparent hover:border-yellow-500 transition-all">
+              Universities
+            </h1>
+          </button>
+    
+          <button onClick={() => window.location.href = ''} className="ml-14 font-serif pr-10">
+            <h1 className="text-4xl text-white hover:border-b-2 border-transparent hover:border-yellow-500 transition-all">
+              Account
+            </h1>
+          </button>
+
+          <a className="block rounded-lg p-1 bg-yellow-500 ring-slate-900/5 shadow-lg space-y-3 hover:bg-yellow-700 ml-14 transition-all">
+            <button onClick={() => window.location.href = ''}>
+              <h1 className="text-4xl text-white font-serif">
+                Subscribe
+              </h1>
+            </button>
+          </a>
+        </div>
+
+        <div className='text-right mt-72 mr-40 font-serif'>
+          <h1 className='text-5xl text-white ml-14'> 
+            Help us open the door 
+            <h2 className='mr-20'>
+              of Knowledge
+            </h2>
+          </h1>
+
+          <div className="flex justify-center ml-96">
+            <a className="inline-block rounded-lg p-1 bg-yellow-500  hover:bg-yellow-700 ml-96 px-10 mt-6 transition-all "> 
+                <button onClick={() => window.location.href = ''}>
+                  <h3 className="text-4xl text-white font-serif">
+                    Wisdom 
+                  </h3>
+                </button>
+              </a>
+          </div>
+        </div>
+        
       </div>
-
-
-      {/* second part of the home page (Science is for everyone)*/}
-      <div className='text-center mt-40 font-serif'>
-        <h1 className='text-5xl text-black'>
-          Science is for Everyone
-          <img className=' mb-96 mx-auto ' src="./src/assets/book.png" alt="book image" />
-        </h1>
-
-       
-      </div>
-      
-     
-
     </>
   );
 }
