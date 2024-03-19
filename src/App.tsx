d6b03c1e1c0f39de061be33b7ed1acedd981335c
diego-rey-2003 <diego.rey2300@gmail.com>
2024-03-19 14:35:29 -0700

    added animations, corrected footer, and tried to fix image problem

--- src/App.tsx
+++ src/App.tsx
@@ -74,9 +74,9 @@ AOS.init({
   return (
     <>
       <section id='Starting-page' ref={startingPageRef}>
-        <div className='w-full h-screen bg-cover bg-no-repeat bg-left-bottom bg-fixed' style={{backgroundImage: "url('../public/assets/animated-light-bulb-gif.webp')"}}>
+        <div className='w-full h-screen bg-cover bg-no-repeat bg-left-bottom bg-fixed' style={{backgroundImage: "url('./assets/animated-light-bulb-gif.webp')"}}>
           <div className="h-20 flex items-center">
-            <button onClick={() => window.location.href = 'http://localhost:5173'}>
+            <button onClick={() => window.location.href = 'https://connectopia-teal.vercel.app/'}>
               <h1 className="text-4xl text-white ml-6 font-sans mr-28">
                 Connectopia
               </h1>
@@ -142,16 +142,16 @@ AOS.init({
           <div className="w-1/2">
             <div className="rounded-lg w-full h-96 shadow-2xl bg-slate-50 flex justify-between items-center ">
               <div className="flex flex-col ">
-                <img src="../public/assets/SFSU.png" alt="SFSU-logo" className='h-32 ml-2 mb-4 ' />
-                <img src="../public/assets/UH-logo.png" alt="UH-logo" className='h-32 ml-3 m-1' />
+                <img src="./assets/sfsu.png" alt="SFSU-logo" className='h-32 ml-2 mb-4 ' />
+                <img src="./assets/UH-logo.png" alt="UH-logo" className='h-32 ml-3 m-1' />
               </div>
               <div className="flex flex-col">
-                <img src="../public/assets/michigan-logo.png" alt="michigan-logo" className='h-32 mb-2 transition ease-in-out delay-100' />
-                <img src="../public/assets/berkeley-logo.png" alt="berkeley-logo" className='h-32 mt-3' />
+                <img src="./assets/michigan-logo.png" alt="michigan-logo" className='h-32 mb-2 transition ease-in-out delay-100' />
+                <img src="./assets/berkeley-logo.png" alt="berkeley-logo" className='h-32 mt-3' />
               </div>
               <div className="flex flex-col">
-                <img src="../public/assets/MIT-logo.png" alt="MIT-logo" className='h-32 mb-2' />
-                <img src="../public/assets/a&m-logo.png" alt="a&m-logo" className='h-32 mr-9 ml-9' />
+                <img src="./assets/MIT-logo.png" alt="MIT-logo" className='h-32 mb-2' />
+                <img src="./assets/a&M-logo.png" alt="a&m-logo" className='h-32 mr-9 ml-9' />
               </div>
             </div>
           </div>
@@ -170,24 +170,22 @@ AOS.init({
       </section>
       <section id='third page' className='h-screen flex items-center justify-center bg-slate-50'>
   <div className='text-center' data-aos="zoom-in">
-    <h1 className='text-7xl mb-8 ml-32 px- font-sans text-left'>
+    <h1 className='text-7xl mb-8 ml-32 px- font-sans text-center'>
       Join thousands of learners and start your knowledge journey today
     </h1>
     <div className="flex justify-center"> 
-      <a className="block rounded-lg p-4 bg-yellow-500 ring-slate-900/5  hover:bg-yellow-400 transition-all shadow-2xl mt-10 mr-5">
+      <a className="block rounded-lg p-4 bg-yellow-500 ring-slate-900/5  hover:bg-yellow-400 transition-all shadow-2xl mt-10 ml-24">
         <button onClick={() => window.location.href = ''} className="text-4xl text-white font-sans">
           Subscribe
         </button>
       </a>
     </div>
   </div>
-<<<<<<< HEAD
-  <img src="./public/assets/secondimage.png" alt="books'image" />
-=======
-  <img src="../public/assets/secondimage.png" alt="" />
->>>>>>> refs/remotes/origin/main
+  <img src="./assets/secondimage.png" alt="books'image" data-aos="zoom-in" />
+  
+
 </section>
-<section className='h-96 bg-black'>
+<section className='h-36 bg-black'>
   <div className='flex items-center'>
     <h1 className='text-white ml-96 mt-4 px-44 font-sans'>
      © 2024 Connectopia™. All Rights Reserved.
